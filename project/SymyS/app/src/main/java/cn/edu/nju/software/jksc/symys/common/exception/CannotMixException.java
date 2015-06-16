78b94f0744a94e7443d767167aa4f08b24463da7
Miau Shiauwe <mxw_19940830@sina.com>
2015-06-16 15:42:43 +0800

    Create class Bobble and some related exceptions

---
+++ project/SymyS/app/src/main/java/cn/edu/nju/software/jksc/symys/common/exception/CannotMixException.java
@@ -0,0 +1,22 @@
+package cn.edu.nju.software.jksc.symys.common.exception;
+
+import cn.edu.nju.software.jksc.symys.common.Bobble;
+
+/**
+ * Created by 缪晓伟 on 2015/6/16.
+ */
+public class CannotMixException extends GameLogicException
+{
+	private Bobble bobble1,bobble2;
+
+	public CannotMixException(Bobble bobble1,Bobble bobble2)
+	{
+		this.bobble1=bobble1;
+		this.bobble2=bobble2;
+	}
+
+	@Override public String toString()
+	{
+		return bobble1+" and "+bobble2+" cannot be mixed!";
+	}
+}
---
+++ project/SymyS/app/src/main/java/cn/edu/nju/software/jksc/symys/common/exception/GameLogicException.java
@@ -0,0 +1,8 @@
+package cn.edu.nju.software.jksc.symys.common.exception;
+
+/**
+ * Created by 缪晓伟 on 2015/6/16.
+ */
+public class GameLogicException extends Exception
+{
+}
