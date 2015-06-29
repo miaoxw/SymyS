e9dc52af90f9495e96dbb5a6f83eac3fce0cf06f
NorviNS <460785372@qq.com>
2015-06-29 20:58:28 +0800

    gamepanel界面更新

--- project/SymyS/app/src/main/java/cn/edu/nju/software/jksc/symys/activities/ScoreActivity.java
+++ project/SymyS/app/src/main/java/cn/edu/nju/software/jksc/symys/activities/ScoreActivity.java
@@ -25,6 +25,7 @@ public class ScoreActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         TextView tx = (TextView)findViewById(R.id.header);
+        TextView best_score = (TextView) findViewById(R.id.best_score);
         Typeface custom_font = Typeface.createFromAsset(getAssets(),
                 "font/HarringtON.TTF");
         tx.setTypeface(custom_font);
@@ -33,6 +34,9 @@ public class ScoreActivity extends Activity {
         tx.setTypeface(custom_font);
 
         tx.setText("" + getIntent().getLongExtra("score", -1L));
+
+        best_score.setTypeface(custom_font);
+        best_score.setText("BestScore:" + "23333");
 
         gameData = (HashMap<String,Object>) getIntent().getSerializableExtra("gameData");
 
